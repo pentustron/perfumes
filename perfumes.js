b05c6606558e835ec1d351624489e66c3c721558
pentustron <148404401+pentustron@users.noreply.github.com>
2026-06-08 17:14:28 -0600

    Add files via upload

---
+++ perfumes.js
@@ -0,0 +1,638 @@
+const perfumes = [
+
+,
+/* LUJO Y EXCLUSIVOS */
+
+{
+name:"Armani Code Elixir",
+category:"lujo",
+age:"25-60",
+season:"Invierno",
+occasion:"Elegante",
+duration:"12h+",
+projection:"Alta",
+price:"$350",
+image:"img/armani-code-elixir.png"
+},
+
+{
+name:"Bvlgari Man In Black",
+category:"lujo",
+age:"25-60",
+season:"Invierno",
+occasion:"Noche",
+duration:"12h+",
+projection:"Alta",
+price:"$350",
+image:"img/bvlgari-man-in-black.png"
+},
+
+{
+name:"Baccarat Rouge 540",
+category:"lujo",
+age:"25-60",
+season:"Todo Año",
+occasion:"Lujo",
+duration:"12h+",
+projection:"Alta",
+price:"$350",
+image:"img/francis-kurkdjian-baccarat-rouge-540.png"
+},
+
+{
+name:"Aventus",
+category:"lujo",
+age:"25-60",
+season:"Todo Año",
+occasion:"Lujo",
+duration:"12h+",
+projection:"Alta",
+price:"$350",
+image:"img/creed-aventus.png"
+},
+
+{
+name:"Millesime Imperial",
+category:"lujo",
+age:"25-60",
+season:"Verano",
+occasion:"Lujo",
+duration:"12h+",
+projection:"Alta",
+price:"$350",
+image:"img/creed-millesime-imperial.png"
+},
+
+{
+name:"Santal 33",
+category:"lujo",
+age:"25-60",
+season:"Todo Año",
+occasion:"Elegante",
+duration:"12h+",
+projection:"Alta",
+price:"$350",
+image:"img/le-labo-santal-33.png"
+},
+
+{
+name:"Tobacolor",
+category:"lujo",
+age:"25-60",
+season:"Invierno",
+occasion:"Lujo",
+duration:"12h+",
+projection:"Alta",
+price:"$350",
+image:"img/christian-dior-tobacolor.png"
+},
+{
+name:"Sauvage Elixir",
+category:"noche",
+age:"20-60",
+season:"Todo Año",
+
+occasion:"Lujo",
+
+duration:"12h+",
+projection:"Extrema",
+price:"$350",
+
+bestSeller:true,
+
+image:"img/christian-dior-sauvage-elixir.png"
+},
+{
+name:"Layton",
+category:"lujo",
+age:"20-55",
+season:"Invierno",
+occasion:"Elegante",
+duration:"12h+",
+projection:"Alta",
+price:"$350",
+image:"img/parfums-de-marly-layton.png"
+},
+
+{
+name:"Erba Pura",
+category:"lujo",
+age:"20-55",
+season:"Primavera",
+occasion:"Lujo",
+duration:"12h+",
+projection:"Alta",
+price:"$350",
+image:"img/xerjoff-erba-pura.png"
+},
+
+{
+name:"Ombre Nomade",
+category:"lujo",
+age:"25-60",
+season:"Invierno",
+occasion:"Lujo",
+duration:"12h+",
+projection:"Extrema",
+price:"$350",
+image:"img/louis-vuitton-ombre-nomade.png"
+},
+
+{
+name:"Ombre Leather",
+category:"lujo",
+age:"25-60",
+season:"Invierno",
+occasion:"Noche",
+duration:"12h+",
+projection:"Alta",
+price:"$350",
+image:"img/tom-ford-ombre-leather.png"
+},
+
+{
+name:"Tobacco Vanille",
+category:"lujo",
+age:"25-60",
+season:"Invierno",
+occasion:"Elegante",
+duration:"12h+",
+projection:"Alta",
+price:"$350",
+image:"img/tom-ford-ombre-tobacco-vanille.png"
+},
+
+/* FRESCOS EXTRA */
+
+{
+name:"Light Blue Man",
+category:"fresco",
+age:"18-50",
+season:"Verano",
+occasion:"Diario",
+duration:"8-10h",
+projection:"Media",
+price:"$350",
+image:"img/dolce-&-gabbana-light-blue-man.png"
+},
+
+{
+name:"Hugo Boss",
+category:"fresco",
+age:"18-50",
+season:"Todo Año",
+occasion:"Oficina",
+duration:"8-10h",
+projection:"Media",
+price:"$350",
+image:"img/hugo-boss.png"
+},
+
+{
+name:"Eros Energy",
+category:"fresco",
+age:"18-40",
+season:"Verano",
+occasion:"Diario",
+duration:"8-10h",
+projection:"Media",
+price:"$350",
+image:"img/versace-eros-energy.jpeg"
+},
+
+/* DULCES EXTRA */
+
+{
+name:"Le Beau",
+category:"dulce",
+age:"18-40",
+season:"Verano",
+occasion:"Citas",
+duration:"10-12h",
+projection:"Alta",
+price:"$350",
+image:"img/jean-paul-gaultier-le-beau.png"
+},
+
+{
+name:"Invictus",
+category:"dulce",
+age:"18-40",
+season:"Todo Año",
+occasion:"Diario",
+duration:"10-12h",
+projection:"Alta",
+price:"$350",
+image:"img/paco-rabanne-invictus.png"
+},
+
+{
+name:"Invictus Platinum",
+category:"dulce",
+age:"18-40",
+season:"Todo Año",
+occasion:"Diario",
+duration:"10-12h",
+projection:"Alta",
+price:"$350",
+image:"img/paco-rabanne-invictus-platinum.png"
+},
+
+{
+name:"Phantom",
+category:"dulce",
+age:"18-40",
+season:"Todo Año",
+occasion:"Diario",
+duration:"10-12h",
+projection:"Alta",
+price:"$350",
+image:"img/paco-rabanne-phantom.png"
+},
+
+/* NOCHE EXTRA */
+
+{
+name:"Born In Roma Intense",
+category:"noche",
+age:"18-45",
+season:"Invierno",
+occasion:"Citas",
+duration:"10-12h",
+projection:"Alta",
+price:"$350",
+image:"img/valentino-born-in-roma-intense.png"
+},
+
+{
+name:"Born In Roma Uomo",
+category:"noche",
+age:"18-45",
+season:"Todo Año",
+occasion:"Elegante",
+duration:"10-12h",
+projection:"Alta",
+price:"$350",
+image:"img/valentino-born-in-roma-oumo.png"
+},
+
+{
+name:"Born In Roma Uomo Extradose",
+category:"noche",
+age:"20-50",
+season:"Invierno",
+occasion:"Noche",
+duration:"10-12h",
+projection:"Alta",
+price:"$350",
+image:"img/valentino-born-in-roma-oumo-extradose.png"
+},
+
+{
+name:"Eros",
+category:"noche",
+age:"18-40",
+season:"Todo Año",
+occasion:"Fiesta",
+duration:"10-12h",
+projection:"Alta",
+price:"$350",
+image:"img/versace-eros.png"
+},
+
+{
+name:"My Self For Him",
+category:"noche",
+age:"20-50",
+season:"Todo Año",
+occasion:"Elegante",
+duration:"10-12h",
+projection:"Alta",
+price:"$350",
+image:"img/yves-saint-laurent-myself-for-him.png"
+},
+/* PERFUMES FALTANTES */
+
+/* ARMANI */
+
+{
+name:"Acqua Di Gio Parfum",
+category:"fresco",
+age:"20-55",
+season:"Verano",
+occasion:"Diario",
+duration:"8-10h",
+projection:"Media",
+price:"$350",
+image:"img/armani-acqua-di-gio-parfum.png"
+},
+
+{
+name:"Stronger With You",
+category:"dulce",
+age:"18-45",
+season:"Invierno",
+occasion:"Citas",
+duration:"10-12h",
+projection:"Alta",
+price:"$350",
+image:"img/armani-stronger-with-you.png"
+},
+
+/* AZZARO */
+
+{
+name:"The Most Wanted",
+category:"noche",
+age:"18-50",
+season:"Invierno",
+occasion:"Fiesta",
+duration:"10-12h",
+projection:"Alta",
+price:"$350",
+image:"img/azzarothe-most-wanted.png"
+},
+
+/* ABERCROMBIE */
+
+{
+name:"Abercrombie",
+category:"fresco",
+age:"18-40",
+season:"Todo Año",
+occasion:"Diario",
+duration:"8-10h",
+projection:"Media",
+price:"$350",
+image:"img/abercrombie.png"
+},
+
+/* BURBERRY */
+
+{
+name:"Burberry",
+category:"fresco",
+age:"18-45",
+season:"Todo Año",
+occasion:"Oficina",
+duration:"8-10h",
+projection:"Media",
+price:"$350",
+image:"img/burberry.png"
+},
+
+/* CAROLINA HERRERA */
+
+{
+name:"212 VIP",
+category:"noche",
+age:"18-45",
+season:"Todo Año",
+occasion:"Fiesta",
+duration:"10-12h",
+projection:"Alta",
+price:"$350",
+image:"img/carolina-212-vip.png"
+},
+
+{
+name:"Bad Boy",
+category:"noche",
+age:"18-45",
+season:"Todo Año",
+occasion:"Fiesta",
+duration:"10-12h",
+projection:"Alta",
+price:"$350",
+image:"img/carolina-herrera-bad-boy.png"
+},
+
+{
+name:"Bad Boy Cobalt",
+category:"noche",
+age:"18-45",
+season:"Todo Año",
+occasion:"Fiesta",
+duration:"10-12h",
+projection:"Alta",
+price:"$350",
+image:"img/carolina-bad-boy-cobalt.png"
+},
+
+{
+name:"Bad Boy Cobalt Electrique",
+category:"noche",
+age:"18-45",
+season:"Todo Año",
+occasion:"Fiesta",
+duration:"10-12h",
+projection:"Alta",
+price:"$350",
+image:"img/carolina-bad-boy-cobalt-elixir.png"
+},
+
+/* JEAN PAUL GAULTIER */
+
+{
+name:"Le Male Elixir",
+category:"dulce",
+age:"18-45",
+season:"Invierno",
+occasion:"Fiesta",
+duration:"12h+",
+projection:"Extrema",
+price:"$350",
+image:"img/jean-paul-gaultier-le-male-elixir.png"
+},
+
+{
+name:"Le Male Le Parfum",
+category:"dulce",
+age:"20-50",
+season:"Invierno",
+occasion:"Noche",
+duration:"10-12h",
+projection:"Alta",
+price:"$350",
+image:"img/jean-paul-gaultier-le-male-elixir-le-parfum.png"
+},
+
+{
+name:"Ultra Male",
+category:"dulce",
+age:"18-40",
+season:"Invierno",
+occasion:"Fiesta",
+duration:"10-12h",
+projection:"Alta",
+price:"$350",
+image:"img/jean-paul-gaultier-ultra-male.png"
+},
+
+{
+name:"Scandal Man",
+category:"dulce",
+age:"18-45",
+season:"Invierno",
+occasion:"Antro",
+duration:"10-12h",
+projection:"Alta",
+price:"$350",
+image:"img/jean-paul-gaultier-scandal-man.png"
+},
+
+/* MONTBLANC */
+
+{
+name:"Explorer",
+category:"fresco",
+age:"18-50",
+season:"Todo Año",
+occasion:"Diario",
+duration:"8-10h",
+projection:"Media",
+price:"$350",
+image:"img/montblanc-explorer.png"
+},
+
+{
+name:"Legend Blue",
+category:"fresco",
+age:"18-45",
+season:"Todo Año",
+occasion:"Oficina",
+duration:"8-10h",
+projection:"Media",
+price:"$350",
+image:"img/montblanc-legend-blue.png"
+},
+
+/* MOSCHINO */
+
+{
+name:"Toy Boy",
+category:"fresco",
+age:"18-45",
+season:"Todo Año",
+occasion:"Citas",
+duration:"8-10h",
+projection:"Media",
+price:"$350",
+image:"img/moschino-toy-boy.png"
+},
+
+/* VERSACE */
+
+{
+name:"Eros",
+category:"noche",
+age:"18-45",
+season:"Todo Año",
+occasion:"Fiesta",
+duration:"8-10h",
+projection:"Alta",
+price:"$350",
+image:"img/versace-eros.png"
+},
+
+{
+name:"Eros Flame",
+category:"noche",
+age:"18-45",
+season:"Invierno",
+occasion:"Citas",
+duration:"8-10h",
+projection:"Alta",
+price:"$350",
+image:"img/versace-eros-flame.png"
+},
+
+/* YSL */
+
+{
+name:"Y",
+category:"fresco",
+age:"18-45",
+season:"Todo Año",
+occasion:"Diario",
+duration:"8-10h",
+projection:"Media",
+price:"$350",
+image:"img/yves-saint-laurent-y.png"
+},
+
+{
+name:"Y Intense",
+category:"fresco",
+age:"20-50",
+season:"Todo Año",
+occasion:"Citas",
+duration:"8-10h",
+projection:"Media",
+price:"$350",
+image:"img/yves-saint-laurent-intense.png"
+},
+
+/* ÁRABES */
+
+{
+name:"Asad",
+category:"dulce",
+age:"18-55",
+season:"Invierno",
+occasion:"Noche",
+duration:"10-12h",
+projection:"Alta",
+price:"$350",
+image:"img/arabes-asad.png"
+},
+
+{
+name:"9PM",
+category:"dulce",
+age:"18-35",
+season:"Invierno",
+occasion:"Fiesta",
+duration:"10-12h",
+projection:"Alta",
+price:"$350",
+image:"img/arabes-9pm.png"
+},
+
+{
+name:"Club De Nuit Intense",
+category:"lujo",
+age:"20-55",
+season:"Todo Año",
+occasion:"Elegante",
+duration:"12h+",
+projection:"Alta",
+price:"$350",
+image:"img/arabes-club-de-nuit-intense.png"
+},
+
+{
+name:"Khamrah Lattafa",
+category:"dulce",
+age:"18-55",
+season:"Invierno",
+occasion:"Citas",
+duration:"12h+",
+projection:"Extrema",
+price:"$350",
+image:"img/arabes-khamrah-lattafa.png"
+},
+{
+name:"360 Red",
+category:"noche",
+age:"18-50",
+season:"Todo Año",
+occasion:"Diario",
+duration:"10-12h",
+projection:"Alta",
+price:"$350",
+image:"img/perry-ellis-360-red.png"
+}
+];
+
+renderPerfumes(perfumes);
